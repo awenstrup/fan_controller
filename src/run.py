b9e39759dda2a9e31eec71153382e9f829710392
Alex Wenstrup <awenstrup@olin.edu>
2020-08-19 13:28:04 -0400

    Finish run script, untested

--- src/run.py
+++ src/run.py
@@ -1,43 +1,56 @@
 """Script to be run on Raspberry Pi
 Periodically takes an image (with attached camera), 
-and uploads it to an S3 bucket.
+and uploads it to an S3 bucket. 
+
+I don't really want label any more data, so I am not adding
+these images to a new training set.
 """
 # Imports
 
 # Exptended Python
 import cv2
 import boto3
+import gpiozero
 
 # Globals
 PERIOD = 10  # 10 seconds
+PIN = 17
 
-def update():
+fan_state: bool = False  # False is off, True is on
+fan = gpiozero.DigitalOutputDevice(17)
+
+def load_model():
+    """Load a pretrained model"""
+    model = keras.models.load(paths.MODEL)
+    return model
+
+def update(model):
+    """Given a model, take a photo and update the fan state"""
     camera = cv2.VideoCapture(0)
     time.sleep(1)
     _, image = camera.read()
     image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
     del(camera)
 
-    
+    prediction = model.predict(image)[0]  # Get only element from list
+    if abs(prediction - 0.5) > 0.1:  
+        # Only update model if the prediction is >60% confident to reduce random toggling
+        temp = True if prediction > 0.5 else False
+        if temp is not fan_state:
+            fan_state = temp
+            toggle_fan()
 
-def upload_s3(file_name: str):
-    """Upload an image to S3
-    
-    Args:
-        file_name (str): The relative path to the image to upload
-        
-    Returns:
-        bool: Whether or not the upload succeeded
-    """
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, "awenstrup", f"tito/{file_name}")
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+def toggle_fan():
+    if fan_state:
+        fan.on()
+    else:
+        fan.off()
 
-while True:
-    file_name = take_picture()
-    upload_s3(file_name)
-    time.sleep(PERIOD)
+if __name__ == "__main__":
+    m = load_model()
+    while True:
+        try:
+            update(m)
+        except:
+            fan.off()
+            break
